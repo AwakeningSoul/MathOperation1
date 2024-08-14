b3837e259315ce84ea2e37770d18def4563f8b0f
AwakeningSoul <weathergoals@gmail.com>
2024-08-14 07:22:56 +0100

    Add project files.

---
+++ MathOperation1/MathOperation1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperation1
+{
+    class MathOperation1
+    {
+        //Create a method that takes two integers as parameters
+        //Make one paramater optional by providing a default value
+        public int MathOperation(int i, int j = 1)
+        {
+            //Perform multiplication on integers and return the result
+            int result = i * j;
+            return result;
+        }
+    }
+}
